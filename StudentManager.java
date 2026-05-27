462c69379cfbde684fb5fb09abdb20d141109f44
A.J.P.M Kushan <malidukushan0421@gmail.com>
2026-05-27 09:49:58 +0530

    Update StudentManager.java

--- StudentManager.java
+++ StudentManager.java
@@ -6,11 +6,30 @@ public class StudentManager {
 
     public void addStudent(Student student){
         students.add(student);
+        System.out.println("Student Added Successfully!");
     }
 
     public void viewStudents(){
         for(Student s : students){
             s.display();
+        }
+    }
+
+    // Search Student
+    public void searchStudent(int id) {
+
+        boolean found = false;
+
+        for (Student s : students) {
+
+            if (s.id == id) {
+                s.display();
+                found = true;
+            }
+        }
+
+        if (!found) {
+            System.out.println("Student Not Found!");
         }
     }
 
