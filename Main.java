6eee911554ad9a8577b8a7e7b444376767ee9eaf
A.J.P.M Kushan <malidukushan0421@gmail.com>
2026-05-27 11:18:03 +0530

    add new added age and other details

--- Main.java
+++ Main.java
@@ -4,8 +4,8 @@ public class Main {
 
         StudentManager manager = new StudentManager();
 
-        Student s1 = new Student(1, "Kushan", "ICT");
-        Student s2 = new Student(2, "Nimal", "SE");
+        Student s1 = new Student(1, "Kushan", "ICT",19,"malidukushan2001@gmail.com",1225466997);
+        Student s2 = new Student(2, "Nimal", "SE",20,"kamal2002@gmail.com",045564446644);
 
         manager.addStudent(s1);
         manager.addStudent(s2);
