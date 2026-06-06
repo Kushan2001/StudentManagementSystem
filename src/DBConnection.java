2458a4bd7e6597f518d8a2010e0d977c1afa01d0
A.J.P.M Kushan <malidukushan0421@gmail.com>
2026-06-07 00:22:59 +0530

    Refactor DBConnection class: streamline connection method and improve error handling

--- src/DBConnection.java
+++ src/DBConnection.java
@@ -1,16 +1,31 @@
 package src;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 
 public class DBConnection {
-    
-        private static final String URL =
-            "jdbc:mysql://localhost:3306/student_management";
 
-        private static final String USER = "root";
+    private static final String URL = "jdbc:mysql://localhost:3306/student_management";
 
-        private static final String PASSWORD = "admin";
+    private static final String USER = "root";
 
+    private static final String PASSWORD = "admin";
 
-         
+    public static Connection getConnection() {
+        try {
+            Connection con = DriverManager.getConnection(
+                    URL,
+                    USER,
+                    PASSWORD);
+
+            return con;
+
+        } catch (Exception e) {
+
+            System.out.println("Database Connection Failed!");
+            e.printStackTrace();
+
+            return null;
+        }
+    }
 }
