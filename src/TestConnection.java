a3bfc210e42e0cfe700cd03513cced1a25a18836
A.J.P.M Kushan <malidukushan0421@gmail.com>
2026-06-07 00:25:22 +0530

    Add TestConnection class to verify database connectivity

--- src/TestConnection.java
+++ src/TestConnection.java
@@ -1,4 +1,14 @@
 package src;
+import java.sql.Connection;
+
 public class TestConnection {
     
+    public static void main(String[] args) {
+
+        Connection con = DBConnection.getConnection();
+
+        if (con != null) {
+            System.out.println("Database Connected Successfully!");
+        }
+    }
 }
