15dd625895259b09030e1ce7b32b425e6ea3489c
A.J.P.M Kushan <malidukushan0421@gmail.com>
2026-06-07 00:18:34 +0530

    Add database connection details to DBConnection class

--- src/DBConnection.java
+++ src/DBConnection.java
@@ -4,5 +4,13 @@ import java.sql.DriverManager;
 
 public class DBConnection {
     
-      
+        private static final String URL =
+            "jdbc:mysql://localhost:3306/student_management";
+
+        private static final String USER = "root";
+
+        private static final String PASSWORD = "admin";
+
+
+         
 }
