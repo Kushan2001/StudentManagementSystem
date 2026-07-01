1986481370640306509231c48cc5192c38e9b836
A.J.P.M Kushan <malidukushan0421@gmail.com>
2026-07-01 20:28:40 +0530

    Enhance student addition functionality with input validation for ID, age, email, and phone number

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,92 @@
 package src;
 
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        
+        Scanner input = new Scanner(System.in);
         StudentManager manager = new StudentManager();
-        
-          Student s1 = new Student(
-                1,
-                "Kushan",
-                "ICT",
-                21,
-                "kushan@gmail.com",
-                "0712345678"
-        );
+
+        int choice;
+
+        do {
+            System.out.println("\n-----------------------");
+            System.out.println("   STUDENT MANAGEMENT SYSTEM");
+            System.out.println("----------------------------");
+            System.out.println("1. Add Student");
+            System.out.println("2. View All Students");
+            System.out.println("3. Search Student by ID");
+            System.out.println("4. Search Student by Name");
+            System.out.println("5. Update Student");
+            System.out.println("6. Delete Student");
+            System.out.println("0. Exit");
+            System.out.println("------------------------------");
+
+            System.out.print("Enter choice: ");
+            choice = input.nextInt();
+            input.nextLine();
+
+            switch (choice) {
+                case 1:
+                    System.out.print("Enter ID: ");
+                    int id = input.nextInt();
+                    input.nextLine();
+
+                    System.out.print("Enter Name: ");
+                    String name = input.nextLine();
+
+                    System.out.print("Enter Course: ");
+                    String course = input.nextLine();
+
+                    System.out.print("Enter Age: ");
+                    int age = input.nextInt();
+                    input.nextLine();
+
+                    System.out.print("Enter Email: ");
+                    String email = input.nextLine();
+
+                    System.out.print("Enter Phone Number: ");
+                    String phone = input.nextLine();
+                    break;
+
+                    if (id <= 0) {
+                        System.out.println("Invalid ID!");
+                        break;
+                    }
+
+                    if (age <= 0) {
+                        System.out.println("Invalid Age!");
+                        break;
+                    }
+
+                    if (!email.contains("@")) {
+                        System.out.println("Invalid Email!");
+                        break;
+                    }
+
+                    if (phone.length() != 10) {
+                        System.out.println("Phone must be 10 digits!");
+                        break;
+                    }
+
+                    Student student = new Student(
+                            id, name, course, age, email, phone);
+
+                    manager.addStudentToDB(student);
+
+                    break;
+                
+
+                case 3:
+
+
+                default:
+                    break;
+            }
+
+        } while (condition);
 
         // INSERT
         manager.addStudentToDB(s1);
@@ -27,9 +98,7 @@ public class Main {
         manager.updateStudentInDB(1, "Kushan Updated", "SE", 25, "malidukushan2001@gmail.com", "04455688");
 
         // DELETE
-        //manager.deleteStudentFromDB(1);
-    }
-       
-    
+        // manager.deleteStudentFromDB(1);
     }
 
+}
