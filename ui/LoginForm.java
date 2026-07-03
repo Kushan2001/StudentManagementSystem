fd437e987f012281a8381e291e4aa5789ea0443b
A.J.P.M Kushan <malidukushan0421@gmail.com>
2026-07-03 14:08:17 +0530

    Implement LoginForm class extending JFrame with basic setup

--- ui/LoginForm.java
+++ ui/LoginForm.java
@@ -3,6 +3,17 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 
-public class LoginForm {
-    
+public class LoginForm extends JFrame {
+    JTextField usernamField;
+    JPasswordField passwordField;
+
+
+    public LoginForm(){
+        setTitle("Student Management System");
+        setSize(350,200);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+
+        
+    }
 }
