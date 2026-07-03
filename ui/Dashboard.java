2d968b8a37928c15239d0e2951231c1ee21487bb
A.J.P.M Kushan <malidukushan0421@gmail.com>
2026-07-03 14:56:38 +0530

    Initialize Dashboard frame with title, size, and close operation

--- ui/Dashboard.java
+++ ui/Dashboard.java
@@ -5,6 +5,12 @@ import java.awt.event.AdjustmentEvent;
 
 public class Dashboard  extends JFrame{
     public Dashboard(){
+        setTitle("Student Management System(Dashboard)");
+        setSize(500,400);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+
+
         
     }
 }
