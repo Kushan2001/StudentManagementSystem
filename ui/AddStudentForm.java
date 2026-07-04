0691716e47c8f03d2b481b85579f7a8e0112b233
A.J.P.M Kushan <malidukushan0421@gmail.com>
2026-07-04 12:45:31 +0530

    Add variable naming inconsistencies in AddStudentForm class

--- ui/AddStudentForm.java
+++ ui/AddStudentForm.java
@@ -8,12 +8,12 @@ import java.awt.event.ActionEvent;
 
 public class AddStudentForm extends JFrame {
     
-    JTextField idDField;
+    JTextField idField;
     JTextField nameField;
     JTextField courseField;
     JTextField ageField;
     JTextField emailField;
-    JTextField PhoneField;
+    JTextField phoneField;
     
     StudentManager manager = new StudentManager();
 
@@ -25,6 +25,33 @@ public class AddStudentForm extends JFrame {
         setLayout(new GridLayout(7,2,10,10) );
 
         //field
+
+        add(new JLabel("ID; "));
+        idField = new JTextField();
+        add(idField);
+
+        add(new JLabel("Name: "));
+        nameField = new JTextField();
+        add(nameField);
+
+        add(new JLabel("Course: "));
+        courseField = new JTextField();
+        add(courseField);
+
+        add(new JLabel("Age: "));
+        ageField = new JTextField();
+        add(ageField);
+
+        add(new JLabel("Email: "));
+        emailField = new JTextField();
+        add(emailField);
+
+        add(new JLabel("ID"));
+        phoneField = new JTextField();
+        add(phoneField);
+
+
+
     }
 
 
