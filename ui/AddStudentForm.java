2a3272286734a449a25f8beb0fcd881fc0d39153
A.J.P.M Kushan <malidukushan0421@gmail.com>
2026-07-04 12:40:02 +0530

    Refactor AddStudentForm to extend JFrame and initialize UI components

--- ui/AddStudentForm.java
+++ ui/AddStudentForm.java
@@ -1,8 +1,31 @@
 package ui;
 import javax.swing.*;
+
+import src.StudentManager;
+
 import java.awt.*;
 import java.awt.event.ActionEvent;
 
-public class AddStudentForm {
+public class AddStudentForm extends JFrame {
     
+    JTextField idDField;
+    JTextField nameField;
+    JTextField courseField;
+    JTextField ageField;
+    JTextField emailField;
+    JTextField PhoneField;
+    
+    StudentManager manager = new StudentManager();
+
+    public AddStudentForm(){
+
+        setTitle("Add Student");
+        setSize(400,400);
+        setLocationRelativeTo(null);
+        setLayout(new GridLayout(7,2,10,10) );
+
+        //field
+    }
+
+
 }
